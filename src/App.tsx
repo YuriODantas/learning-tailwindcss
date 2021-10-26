4264e8ba344d59f8c3367f6d8d8435e537ad203b
Yuri de Oliveira Dantas <iruy3343@gmail.com>
2021-10-26 20:33:43 -0300

    Create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { TestimonialItem } from "./components/TestimonialItem";
+
 function App() {
   return (
-    <div>
-      Olá mundo!
+    <div className="p-10">
+      <TestimonialItem />
+      <a 
+        className="block shadow bg-green-600 text-white text-center p-4 mt-5 rounded-xl hover:bg-green-500"
+        href="https://www.linkedin.com/in/yuriodantas/">
+          Quero falar com o Yuri
+        </a>
     </div>
   );
 }
